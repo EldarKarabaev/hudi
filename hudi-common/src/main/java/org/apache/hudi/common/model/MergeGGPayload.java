e8540f21949da43b2e5ccdef2c3287b8e45fc5f0
Eldar Karabaev <eldar.karabaev@sixt.com>
2021-07-22 17:08:10 +0200

    V2: using recordBytes accessing myAvroBytes

--- hudi-common/src/main/java/org/apache/hudi/common/model/MergeGGPayload.java
+++ hudi-common/src/main/java/org/apache/hudi/common/model/MergeGGPayload.java
@@ -91,7 +91,7 @@ public class MergeGGPayload extends BaseAvroPayload
       //newValidityMap.put("pos: ", origPosString);
       //((GenericRecord) indexedRecord).put(GG_VALIDITY_MAP_COLUMN_NAME, newValidityMap);
       //((Map)((GenericRecord) indexedRecord).get(GG_DATA_MAP_COLUMN_NAME)).put("DIRECT_PUT","VALUE");
-      ((GenericRecord) indexedRecord).put("feld","UPD. feld");
+      ((GenericRecord) indexedRecord).put("feld","recordBytes:" + recordBytes.length + ", myAvroBytes:" + myAvroBytes.length);
       return Option.of(indexedRecord);
     }
   }
